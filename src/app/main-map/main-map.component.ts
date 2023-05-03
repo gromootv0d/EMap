7dae1a80bf5a304f06af7c0092f955eb1fbf45c4
Maksim <bee.does.bzz@gmail.com>
2023-05-04 00:36:23 +0300

    added custom marker by right click

--- src/app/main-map/main-map.component.ts
+++ src/app/main-map/main-map.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 import * as L from 'leaflet';
 
 @Component({
@@ -8,15 +9,17 @@ import * as L from 'leaflet';
 })
 export class MainMapComponent {
   private map: any;
-  private lat = 51.678418;
-  private lng = 7.809007;
-  private zoom = 14;
+  private marker: any;
+  private lat = 55.751244;
+  private lng = 37.618423;
+  private zoom = 13;
 
   ngOnInit(): void {
     this.initMap();
   }
 
   private initMap(): void {
+    
     this.map = L.map('map', {
       center: [this.lat, this.lng],
       zoom: this.zoom,
@@ -26,5 +29,19 @@ export class MainMapComponent {
       attribution: ''
     }).addTo(this.map);
     this.map.invalidateSize();
+
+    this.map.on('contextmenu', (event: L.LeafletMouseEvent) => {
+      
+      const customIcon = L.icon({
+        iconUrl: 'https://cdn-icons-png.flaticon.com/512/446/446075.png',
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -40],
+      });
+      const markerOptions = {
+        icon: customIcon,
+      };
+      const marker = L.marker(event.latlng, markerOptions).addTo(this.map);
+    });
   }
 }
